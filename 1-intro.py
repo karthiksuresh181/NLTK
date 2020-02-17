a78f1fb615378ebeec925cfe2e110d4ad8507684
Karthik Suresh <Karthik.Suresh@yti.yokogawa.com>
2020-02-17 10:13:30 +0530

    'test_comment_removed'

--- 1-intro.py
+++ 1-intro.py
@@ -3,6 +3,3 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 e_text = "Hello Mr. Smith, how are you doing today? The weather is great and Python is awesome. The sky is pinkish-blue. You should not eat rubber."
 print(sent_tokenize(e_text))
 print(word_tokenize(e_text))
-
-
-#test comment
